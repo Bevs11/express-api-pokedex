fe014737b710dfcfeced06c95655ae742e4f3f48
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-06-23 18:18:01 +0800

    fix: hidden uri into env

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const server = express();
 const PORT = 8000;
+
+dotenv.config();
 
 // Use our modules
 server.use(bodyParser.json()); // This solves getting the body of the request
@@ -13,7 +16,7 @@ server.use(cors()); // Solves communication by other software
 const baseURL = "/api/v1/pokemons";
 const PokemonRoutes = require("./routes/PokemonRoutes.js");
 
-mongoose.connect(MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 server.get("/", (request, response) => {
   console.log(`Online on port 8000`);
